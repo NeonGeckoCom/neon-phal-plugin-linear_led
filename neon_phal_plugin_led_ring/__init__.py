d1597cd1096615cd37bf51b60991f34a043f11de
Daniel McKnight <daniel@neon.ai>
2022-11-04 11:08:05 -0700

    Add locking around LED animations to prevent overlap Add messagebus API to display arbitrary animations

--- neon_phal_plugin_led_ring/__init__.py
+++ neon_phal_plugin_led_ring/__init__.py
@@ -26,11 +26,12 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from threading import RLock
 from ovos_utils.log import LOG
 from ovos_plugin_manager.templates.phal import PHALPlugin
 from abstract_hardware_interface.led import Color, AbstractLed
-from abstract_hardware_interface.led.animations import BreatheLedAnimation,\
-    LoopFillLedAnimation
+from abstract_hardware_interface.led.animations import BreatheLedAnimation, \
+    LoopFillLedAnimation, animations, LedAnimation
 
 
 class LedRing(PHALPlugin):
@@ -42,7 +43,9 @@ class LedRing(PHALPlugin):
         self.listen_color = Color.WHITE
         self.mute_color = Color.BURNT_ORANGE
         self.sleep_color = Color.RED
+        self.init_settings()
 
+        self._led_lock = RLock()
         self.listen_timeout_sec = 30
 
         self._listen_animation = BreatheLedAnimation(self.leds,
@@ -59,19 +62,59 @@ class LedRing(PHALPlugin):
 
         self.register_listeners()
 
+    def init_settings(self):
+        """
+        Safely initialize LED color settings from config.
+        """
+        try:
+            self.listen_color = Color.from_name(
+                self.config.get('listen_color') or 'white')
+        except ValueError:
+            LOG.warning(f'invalid color in config: '
+                        f'{self.config.get("listen_color")}')
+            self.listen_color = Color.WHITE
+
+        try:
+            self.mute_color = Color.from_name(
+                self.config.get('mute_color') or 'burnt_orange')
+        except ValueError:
+            LOG.warning(f'invalid color in config: '
+                        f'{self.config.get("mute_color")}')
+            self.listen_color = Color.BURNT_ORANGE
+
+        try:
+            self.sleep_color = Color.from_name(
+                self.config.get('sleep_color') or 'red')
+        except ValueError:
+            LOG.warning(f'invalid color in config: '
+                        f'{self.config.get("sleep_color")}')
+            self.listen_color = Color.RED
+
     def register_listeners(self):
         self.bus.on('mycroft.mic.mute', self.on_mic_mute)
         self.bus.on('mycroft.mic.unmute', self.on_mic_unmute)
         self.bus.on('mycroft.volume.increase', self.on_volume_increase)
         self.bus.on('mycroft.volume.decrease', self.on_volume_decrease)
+        self.bus.on('neon.led_ring.show_animation', self.on_show_animation)
+
+    def on_show_animation(self, message):
+        animation_name = message.data.get('animation')
+        color_name = message.data.get('color')
+        timeout = message.data.get('timeout', 5)
+        color = Color.from_name(color_name)
+        animation: LedAnimation = animations[animation_name](self.leds, color)
+        with self._led_lock:
+            animation.start(timeout)
 
     def on_mic_mute(self, message):
         LOG.debug('muted')
-        self._mute_animation.start()
+        with self._led_lock:
+            self._mute_animation.start()
 
     def on_mic_unmute(self, message):
         LOG.debug('unmuted')
-        self._unmute_animation.start()
+        with self._led_lock:
+            self._unmute_animation.start()
 
     def on_volume_increase(self, message):
         # TODO: Get volume and fill LEDs accordingly
@@ -83,17 +126,20 @@ class LedRing(PHALPlugin):
 
     def on_record_begin(self, message=None):
         LOG.debug('record begin')
-        self._listen_animation.start(self.listen_timeout_sec)
+        with self._led_lock:
+            self._listen_animation.start(self.listen_timeout_sec)
 
     def on_record_end(self, message=None):
         LOG.debug('record end')
         self._listen_animation.stop()
 
     def on_awake(self, message=None):
-        self._awake_animation.start()
+        with self._led_lock:
+            self._awake_animation.start()
 
     def on_sleep(self, message=None):
-        self._sleep_animation.start()
+        with self._led_lock:
+            self._sleep_animation.start()
 
     def on_reset(self, message=None):
         self.leds.fill(Color.BLACK)
