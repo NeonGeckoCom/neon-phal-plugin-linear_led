6dbe9f9d54f673cee0ca7bfa48ba1846ace35ad5
Daniel McKnight <daniel@neongecko.com>
2022-12-09 10:03:45 -0800

    Track internet connection via messagebus and animate on disconnection if internet is expected

--- neon_phal_plugin_linear_led/__init__.py
+++ neon_phal_plugin_linear_led/__init__.py
@@ -34,6 +34,7 @@ from ovos_plugin_manager.templates.phal import PHALPlugin
 from ovos_plugin_manager.hardware.led import Color, AbstractLed
 from ovos_plugin_manager.hardware.led.animations import BreatheLedAnimation, \
     FillLedAnimation, BlinkLedAnimation, animations, LedAnimation
+from ovos_utils.network_utils import is_connected
 
 
 class LinearLed(PHALPlugin):
@@ -41,6 +42,9 @@ class LinearLed(PHALPlugin):
         PHALPlugin.__init__(self, bus=bus, name=name, config=config)
         self.leds = led
         self.leds.fill(Color.BLACK.as_rgb_tuple())
+
+        self._is_muted = False
+        self._internet_disconnected = not is_connected()
 
         self.listen_color = Color.THEME
         self.mute_color = Color.BURNT_ORANGE
@@ -79,6 +83,9 @@ class LinearLed(PHALPlugin):
                                                          self.error_color,
                                                          4, False)
 
+        self._disconnected_animation = BreatheLedAnimation(self.leds,
+                                                           self.error_color)
+
         self.register_listeners()
 
         # Get theme colors
@@ -86,6 +93,10 @@ class LinearLed(PHALPlugin):
 
         # Check mic switch status
         self.bus.emit(Message('mycroft.mic.status'))
+
+        # Start internet animation
+        if self._internet_disconnected:
+            self.on_no_internet()
 
         # TODO: Define a queue for animations to handle synchronous animations
         #       and restoring persistent states
@@ -145,6 +156,9 @@ class LinearLed(PHALPlugin):
         self.bus.on('mycroft.volume.increase', self.on_volume_increase)
         self.bus.on('mycroft.volume.decrease', self.on_volume_decrease)
 
+        # Internet event handler
+        self.bus.on('mycroft.internet.connected', self.on_internet_connected)
+
         # Core API handlers
         self.bus.on('neon.linear_led.show_animation', self.on_show_animation)
         self.bus.on('ovos.theme.get.response', self.on_theme_update)
@@ -156,6 +170,22 @@ class LinearLed(PHALPlugin):
         self.bus.on('mycroft.speech.recognition.unknown',
                     self.on_recognition_unknown)
         # TODO: Define method to stop any active/queued animations
+
+    def on_no_internet(self, message=None):
+        self._internet_disconnected = True
+        message = message.forward("ovos.phal.wifi.plugin.status") if \
+            message else Message("ovos.phal.wifi.plugin.status")
+        resp = self.bus.wait_for_response(message)
+        if resp and not resp.data.get('watchdog_active'):
+            LOG.info(f"Watchdog inactive: {resp.data}")
+            return
+        # TODO: Check ready settings and skill internet setting in config?
+        while self._internet_disconnected:
+            with self._led_lock:
+                self._disconnected_animation.start()
+
+    def on_internet_connected(self, message):
+        self._internet_disconnected = False
 
     def on_complete_intent_failure(self, message):
         with self._led_lock:
@@ -211,11 +241,13 @@ class LinearLed(PHALPlugin):
     def on_mic_mute(self, message):
         LOG.debug('muted')
         with self._led_lock:
+            self._is_muted = True
             self._mute_animation.start()
 
     def on_mic_unmute(self, message):
         LOG.debug('unmuted')
         with self._led_lock:
+            self._is_muted = False
             self._unmute_animation.start()
 
     def on_volume_increase(self, message):
