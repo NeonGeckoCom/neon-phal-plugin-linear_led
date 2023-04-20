1fab9ac0599ebd1b1c46846a658364c7dd60c22c
Daniel McKnight <daniel@neongecko.com>
2023-04-20 15:55:04 -0700

    Update automation to shared actions

--- setup.py
+++ setup.py
@@ -31,38 +31,40 @@ from os import path, getenv
 
 PLUGIN_ENTRY_POINT = "neon-phal-plugin-linear-led-smbus=neon_phal_plugin_linear_led.smbus_led:SmbusLinearLed"
 ADMIN_PLUGIN_ENTRY_POINT = "neon-phal-plugin-linear-led-neopixel=neon_phal_plugin_linear_led.neopixel_led:NeopixelLinearLed"
+BASE_PATH = path.abspath(path.dirname(__file__))
 
-BASEDIR = path.abspath(path.dirname(__file__))
+
+def get_requirements(requirements_filename: str):
+    requirements_file = path.join(BASE_PATH, requirements_filename)
+    with open(requirements_file, 'r', encoding='utf-8') as r:
+        requirements = r.readlines()
+    requirements = [r.strip() for r in requirements if r.strip()
+                    and not r.strip().startswith("#")]
+
+    for i in range(0, len(requirements)):
+        r = requirements[i]
+        if "@" in r:
+            parts = [p.lower() if p.strip().startswith("git+http") else p
+                     for p in r.split('@')]
+            r = "@".join(parts)
+        if getenv("GITHUB_TOKEN"):
+            if "github.com" in r:
+                requirements[i] = \
+                    r.replace("github.com",
+                              f"{getenv('GITHUB_TOKEN')}@github.com")
+    return requirements
 
 
-with open("./version.py", "r", encoding="utf-8") as v:
+with open(path.join(BASE_PATH, "README.md"), "r") as f:
+    long_description = f.read()
+
+with open(path.join(BASE_PATH, "version.py"), "r", encoding="utf-8") as v:
     for line in v.readlines():
         if line.startswith("__version__"):
             if '"' in line:
                 version = line.split('"')[1]
             else:
                 version = line.split("'")[1]
-
-with open("README.md", "r") as f:
-    long_description = f.read()
-
-
-def get_requirements(requirements_filename: str):
-    requirements_file = path.join(path.abspath(path.dirname(__file__)), "requirements", requirements_filename)
-    with open(requirements_file, 'r', encoding='utf-8') as r:
-        requirements = r.readlines()
-    requirements = [r.strip() for r in requirements if r.strip() and not r.strip().startswith("#")]
-
-    for i in range(0, len(requirements)):
-        r = requirements[i]
-        if "@" in r:
-            parts = [p.lower() if p.strip().startswith("git+http") else p for p in r.split('@')]
-            r = "@".join(parts)
-            if getenv("GITHUB_TOKEN"):
-                if "github.com" in r:
-                    r = r.replace("github.com", f"{getenv('GITHUB_TOKEN')}@github.com")
-            requirements[i] = r
-    return requirements
 
 
 setup(
